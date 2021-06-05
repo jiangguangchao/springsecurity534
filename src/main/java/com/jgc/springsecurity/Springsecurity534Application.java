16c7da1ebf2e85dd745f7d8635b16878ba413ea4
jgc <jiangguangchao157@163.com>
2021-06-05 08:04:27 +0800

    初始化项目，springboot 引入spring security 并且未做任何配置

---
+++ src/main/java/com/jgc/springsecurity/Springsecurity534Application.java
@@ -0,0 +1,13 @@
+package com.jgc.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurity534Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurity534Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgc/springsecurity/Springsecurity534ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgc.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurity534ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
