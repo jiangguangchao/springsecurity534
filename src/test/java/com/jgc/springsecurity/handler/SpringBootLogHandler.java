d84b6ed3a4e4fb6579299c20c324259066834aac
jgc_work <jiangguangchao157@163.com>
2021-09-01 14:26:03 +0800

    完成springboot启动日志解析

--- src/test/java/com/jgc/springsecurity/handler/SpringBootLogHandler.java
+++ src/test/java/com/jgc/springsecurity/handler/SpringBootLogHandler.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -17,10 +18,10 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * @program: springsecurity534
@@ -34,29 +35,45 @@ public class SpringBootLogHandler {
 
     @Test
     public void  handlerLog() {
-        String start = "Creating shared instance of singleton bean";
+        String start = "Creating instance of bean";
         String end = "Finished creating instance of bean";
+
+        //xml文件生成位置
         String filePath = "C:\\Users\\Administrator\\Desktop\\111.txt";
 
         BufferedReader br = null;
+        List<String> list = new ArrayList<String>();
         try {
             br = new BufferedReader(new FileReader(filePath));
             String line = br.readLine();
             Document document = getDocument();
             Element root = document.createElement("beans");
-            Element parent = root;
+
+            int m = 0;
+            list.add("beans" + "___" + m);
+            Element current = root;
             while (line != null) {
                 if (line.contains(start)) {
 
                     String beanStr = getBeanName(line,start);
                     if (StringUtils.isNotBlank(beanStr)) {
-                        log.info("读取到bean[{}], parent[{}]" , beanStr, parent.getNodeName());
+                        log.info("读取到bean[{}] 开始创建, current[{}]" , beanStr, current != null ? current.getNodeName() : "空");
+                        m++;
+                        list.add(beanStr + "___" + m);
                         Element bean = document.createElement(beanStr);
-                        parent.appendChild(bean);
-                        parent = bean;
+                        current.appendChild(bean);
+                        current = bean;
                     }
                 } else if (line.contains(end)) {
-                    parent = (Element) parent.getParentNode();
+                    String beanStr = getBeanName(line,end);
+                    list.add(beanStr + "___" + m);
+                    m--;
+                    String currentNodeName = current.getNodeName();
+                    if (!beanStr.equals(currentNodeName)) {
+                        log.error("当前bean名称[{}]和节点名称[{}]不一致", beanStr, currentNodeName);
+                    }
+                    current = (Element) current.getParentNode();
+                    log.info("当前bean[{}]创建完成,current变为[{}]", beanStr,current != null ? current.getNodeName() : "空");
                 }
                 line = br.readLine();
             }
@@ -64,6 +81,39 @@ public class SpringBootLogHandler {
             writeXmlToFile(document, null);
         } catch (Exception e) {
             log.error("日志文件解析异常", e);
+
+        } finally {
+
+
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            /**
+             * 排查问题代码，如果不能正常生成xml文件，可以这段代码排查
+             */
+//            if (!CollectionUtils.isEmpty(list)) {
+//                String path = "D:\\logs\\list.txt";
+//                try {
+//                    BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path)));
+//                    for (String s : list) {
+//                        if (StringUtils.isBlank(s)) {
+//                            continue;
+//                        }
+//
+//                        String str = getBeanNameStr(s);
+//                        bw.write(str);
+//                        bw.newLine();
+//                    }
+//                    bw.flush();
+//                    bw.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
         }
     }
 
@@ -77,13 +127,24 @@ public class SpringBootLogHandler {
         int n3 = line.indexOf("'", n2 + 1);
         if (n1 > 0 && n2 > 0 && n2 > n1) {
             String beanStr = line.substring(n2 + 1, n3);
-            beanStr = beanStr.replace("#", "_");
-            beanStr = beanStr.replace("$", "-");
+            beanStr = beanStr.replace("#", "_J_");
+            beanStr = beanStr.replace("$", "_S_");
             return beanStr;
         }
 
         return null;
 
+    }
+
+    public String getBeanNameStr(String s) {
+        String[] arr = s.split("___");
+        String beanName = arr[0];
+        int n = Integer.valueOf(arr[1]);
+        String ss = "";
+        for (int i = 0; i < n; i++) {
+            ss = ss + "-";
+        }
+        return ss + beanName;
     }
 
     @Test
