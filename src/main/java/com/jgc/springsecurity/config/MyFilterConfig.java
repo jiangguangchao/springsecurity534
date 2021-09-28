65d4d384729361cd79def76f94bb93b6a102e2f6
jgc_work <jiangguangchao157@163.com>
2021-09-28 15:20:49 +0800

    解决上次提交遗留的一个问题，现在可以使自定义过滤器在spring security过滤器之前执行了

--- src/main/java/com/jgc/springsecurity/config/MyFilterConfig.java
+++ src/main/java/com/jgc/springsecurity/config/MyFilterConfig.java
@@ -30,10 +30,21 @@ public class MyFilterConfig {
     }
 
 
+    /**
+     * 使用Order 注解不能决定过滤器的先后顺序，这个Order注解的作用
+     * 应该是表示bean的先后顺序吧。所以要决定filter的顺序，需要调用
+     * DelegatingFilterProxyRegistrationBean对象的setOrder方法
+     * 设置order的值。
+     * 比如这里设置为-200，因为spring security默认是-100，所以就可以使
+     * 自定义过滤器在spring security过滤器前执行。实际效果，也确实这样。
+     *
+     * @return
+     */
     @Bean
-    @Order(1)
+//    @Order(-200)
     public DelegatingFilterProxyRegistrationBean myFilterRegistration() {
         DelegatingFilterProxyRegistrationBean bean = new DelegatingFilterProxyRegistrationBean("getFiltera");
+        bean.setOrder(-200);
         return bean;
     }
 
