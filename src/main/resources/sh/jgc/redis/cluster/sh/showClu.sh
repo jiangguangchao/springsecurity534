34d38163168f4f1e7b3d837e1661edceec257c88
jgc_work <jiangguangchao157@163.com>
2022-01-06 16:46:40 +0800

    显示集群ip关系脚本

--- src/main/resources/sh/jgc/redis/cluster/sh/showClu.sh
+++ src/main/resources/sh/jgc/redis/cluster/sh/showClu.sh
@@ -16,9 +16,11 @@ for (( i = 0; i < $master_size; i++ )); do
 #    myMap[$i]=`echo ${master_ip_port_arr[i]%@*}`
     master_id=${master_id_arr[$i]}
     master_ip=${master_ip_port_arr[$i]}
+    if [[ $master_ip == 10.0* ]]; then
+        master_ip=$HOST
+    fi
     slave_ip_for_master=""
     for (( j = 0; j < $slave_size ; j++ )); do
-#        myMap[$i]=${myMap[$i]}${slave_ip_port_arr[$j]}
         if [ ${master_id} == ${master_id_in_slave_arr[$j]} ]; then
 #            echo "找到一个从节点 master_id:$master_id , ip_host:${master_ip_port_arr[$i]}, slave_ip_port:${slave_ip_port_arr[$j]}"
             slave_ip_for_master=${slave_ip_for_master}${slave_ip_port_arr[$j]%@*}", "
